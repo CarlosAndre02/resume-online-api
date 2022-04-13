a3b53dfcb53a53e4c1e8948f544f2a33d05c4342
Carlos André <cagd2002@gmail.com>
2022-04-13 15:37:57 -0300

    chore: Add sequelize and database config

---
+++ src/config/database.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+module.exports = {
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  dialect: process.env.DATABASE_DIALECT,
+  dialectOptions: {
+    timezone: 'America/Sao_Paulo',
+  },
+  timezone: 'America/Sao_Paulo',
+
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+};
+
+/* {
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: "database_development",
+    host: DB_HOST,
+    dialect: "postgres",
+}
+}
+*/
--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require('./database/index');
+
 // Routes
 const userRoutes = require('./routes/user');
 const authenticateRoutes = require('./routes/authenticate');
@@ -13,5 +15,5 @@ const authenticateRoutes = require('./routes/authenticate');
 app.use(userRoutes);
 app.use(authenticateRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.APP_PORT || 3000;
 app.listen(port, () => console.log(`Acessar http://localhost:${port}`));
