747e50b5b2144ba1f0fefa841eefb9f97a473136
Carlos André <cagd2002@gmail.com>
2022-04-14 14:58:54 -0300

    feat: Add list users feature

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,14 @@
 const User = require('../models/User');
 
 module.exports = {
+  async index(req, res) {
+    try {
+      const users = await User.findAll({ attributes: ['id', 'username'] });
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  },
   async create(req, res) {
     try {
       const newUser = await User.create(req.body);
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,7 @@ const router = Router();
 
 const userController = require('../controllers/user');
 
+router.get('/users', userController.index);
 router.post('/users', userController.create);
 
 module.exports = router;
