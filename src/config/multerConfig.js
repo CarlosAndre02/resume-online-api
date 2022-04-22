b4fbbd21cd324f8cbfd726c74f55a7e6cc27c9ce
Carlos André <cagd2002@gmail.com>
2022-04-22 20:41:34 -0300

    chore: Add multer and aws s3 configs

---
+++ src/config/multerConfig.js
@@ -0,0 +1,38 @@
+const multerS3 = require('multer-s3');
+const crypto = require('crypto');
+const aws = require('aws-sdk');
+
+module.exports = {
+  storage: multerS3({
+    s3: new aws.S3(),
+    bucket: process.env.S3_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
+        const fileName = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, fileName);
+      });
+    },
+  }),
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png',
+      'image/gif',
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Tipo de arquivo inválido'));
+    }
+  },
+};
