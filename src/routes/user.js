590f206893250fae1a50144e4d3a276b35000186
Carlos André <cagd2002@gmail.com>
2022-04-23 21:56:57 -0300

    feat: Add authenticate middleware to user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,8 +7,8 @@ const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 router.get('/users', ensureAuthenticated, userController.index);
 router.post('/users', userController.create);
-router.get('/users/:id', userController.show);
-router.put('/users/:id', userController.update);
-router.delete('/users/:id', userController.delete);
+router.get('/users/:id', ensureAuthenticated, userController.show);
+router.put('/users/:id', ensureAuthenticated, userController.update);
+router.delete('/users/:id', ensureAuthenticated, userController.delete);
 
 module.exports = router;
