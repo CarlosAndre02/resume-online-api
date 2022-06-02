60cbb58fa69cfabc9e2a2376e62fc58507b6053d
Carlos André <cagd2002@gmail.com>
2022-06-02 12:31:37 -0300

    Change database url env

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  host: process.env.DATABASE_HOST,
+  host: process.env.DATABASE_URL,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
