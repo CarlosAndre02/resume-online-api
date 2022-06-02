9f67c4bba1541b45a4c63b15fd5d0813a3da6e7f
Carlos André <cagd2002@gmail.com>
2022-06-02 15:14:25 -0300

    Update sequelize config

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  host: process.env.DATABASE_URL,
+  host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
